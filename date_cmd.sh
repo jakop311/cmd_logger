4d53f37e4525def8908619959dd0e5930a327ced
jakop311 <nomura@cysec.cs.ritsumei.ac.jp>
2024-10-28 23:23:40 +0900

    Added a script for adding Date&Time beside the username@hostname for better logging

---
+++ date_cmd.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+#----------------------------------------------------#
+#--------- Modified Prompt with Date and Time -------#
+#----------------------------------------------------#
+configure_prompt() {
+    prompt_symbol=㉿
+    # Skull emoji for root terminal
+    #[ "$EUID" -eq 0 ] && prompt_symbol=💀
+    case "$PROMPT_ALTERNATIVE" in
+        twoline)
+		PROMPT=$'%F{%(#.blue.green)}┌──%F{white}[%D{%Y-%m-%d %H:%M:%S}]%F{reset} %F{%(#.blue.green)}${debian_chroot:+($debian_chroot)─}${VIRTUAL_ENV:+($(basename $VIRTUAL_ENV))─}(%B%F{%(#.red.blue)}%n'$prompt_symbol$'%m%b%F{%(#.blue.green)})-[%B%F{reset}%(6~.%-1~/…/%4~.%5~)%b%F{%(#.blue.green)}]\n└─%B%(#.%F{red}#.%F{blue}$)%b%F{reset} '
+            # Right-side prompt with exit codes and background processes
+            #RPROMPT=$'%(?.. %? %F{red}%B⨯%b%F{reset})%(1j. %j %F{yellow}%B⚙%b%F{reset}.)'
+            ;;
+        oneline)
+            PROMPT=$'%F{white}[%D{%Y-%m-%d %H:%M:%S}]%F{reset} ${debian_chroot:+($debian_chroot)}${VIRTUAL_ENV:+($(basename $VIRTUAL_ENV))}%B%F{%(#.red.blue)}%n@%m%b%F{reset}:%B%F{%(#.blue.green)}%~%b%F{reset}%(#.#.$) '
+            RPROMPT=
+            ;;
+        backtrack)
+            PROMPT=$'%F{white}[%D{%Y-%m-%d %H:%M:%S}]%F{reset} ${debian_chroot:+($debian_chroot)}${VIRTUAL_ENV:+($(basename $VIRTUAL_ENV))}%B%F{red}%n@%m%b%F{reset}:%B%F{blue}%~%b%F{reset}%(#.#.$) '
+            RPROMPT=
+            ;;
+    esac
+    unset prompt_symbol
+}
